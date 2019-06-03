91dd58fc0c4e53a3224c454d950773adc36a8a50
Luca Avino <luca.avino@virgilio.it>
2019-06-03 16:29:06 +0200

    tabella

--- Scripts/Script.sql
+++ Scripts/Script.sql
@@ -33,3 +33,32 @@ VALUES (7, 'Pizzeria da Tony', '42.70,14.56',100,'pizza','tonytony@live.it');
 insert into green_ristoranti (ristorante_id, nome, posizione, posti_disponibili,tipo_cucina,email)
 VALUES (8, 'Pizzeria da Tommy', '40.75,13.56',100,'pizza','tommasino@live.it');
 
+
+
+--TABELLA UTENTI
+DROP TABLE Green_Utenti;
+CREATE TABLE Green_Utenti (
+utente_id integer
+constraint Green_Utenti_pk primary key,
+nome varchar2(20)
+constraint utenti_nome_nn not null,
+cognome varchar2(20)
+constraint utenti_cognome_nn not null,
+email varchar2(20)
+constraint utenti_email_nn not null,
+user_id varchar2(20),
+password_user varchar2(20)
+constraint utenti_password_nn not null,
+affidabilita varchar2(20));
+
+insert into Green_Utenti(utente_id, nome, cognome, email,user_id,password_user,affidabilita)
+VALUES (1, 'Vincenzo', 'Ammirati','vinx718@gmail.com','1','c140','5');
+
+insert into Green_Utenti(utente_id, nome, cognome, email,user_id,password_user,affidabilita)
+VALUES (2, 'Luca', 'Avino','luk@gmail.com','2','c140l','1');
+
+insert into Green_Utenti(utente_id, nome, cognome, email,user_id,password_user,affidabilita)
+VALUES (3, 'Antonio', 'Poidomani','poid@gmail.com','3','c140p','4');
+
+insert into Green_Utenti(utente_id, nome, cognome, email,user_id,password_user,affidabilita)
+VALUES (4, 'Tommaso', 'Pavan','vtommy@gmail.com','4','c140t','3');
