74733b9fc1730228cef7e54f5e69c5dad5ca9aae
Luca Avino <luca.avino@virgilio.it>
2019-06-03 15:04:56 +0200

    ok

--- src/main/java/dd/OvedController.java
+++ src/main/java/dd/OvedController.java
@@ -1,12 +1,10 @@
 package dd;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 
@@ -18,8 +16,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class OvedController {
-	private static final Logger logger = LoggerFactory.getLogger(OvedController.class);
-	// …
+	
 	
 	@Autowired Green_RistorantiRepo repo;
 	
@@ -56,6 +53,28 @@ public class OvedController {
 	}
 	
 	
+	@GetMapping("/update")
+	public String updateRistoranti() {
+		
+		return "/modifica";
+	}
+	
+	
+	@GetMapping("/modifica")
+	public String modificaRistoranti(@RequestParam(name = "id") long ristoranteId, 
+			@RequestParam(name = "nome") String nome, @RequestParam(name = "posizione") String posizione,
+			@RequestParam(name = "postiDisponibili") long postiDisponibili, 
+			@RequestParam(name = "tipoCucina") String tipoCucina,
+			@RequestParam(name="email") String email,
+			
+			Model model) {
+		Green_Ristorante risto= new Green_Ristorante(ristoranteId, nome, posizione, postiDisponibili,  tipoCucina,
+			email);
+		repo.save(risto);
+		model.addAttribute("restaurants", repo.findAll()); /* dopo aver inserito l'elemento dall'oggetto,
+		devo ripassare i valori nell'oggetto repo con findAll */
+		return "/elenco";
+	}
 	
 	
 	
