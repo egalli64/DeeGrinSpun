7e35f307e56a58c1589ed4145788b3246fda9c1f
Luca Avino <luca.avino@virgilio.it>
2019-06-03 16:33:56 +0200

    ok

--- src/main/java/dd/Green_Utenti.java
+++ src/main/java/dd/Green_Utenti.java
@@ -8,21 +8,86 @@ import javax.persistence.Table;
 	
 
 @Entity
-@Table(name = "GREEN_RISTORANTI")
+@Table(name = "GREEN_UTENTI")
 public class Green_Utenti {
 @Id
-@Column(name = "RISTORANTE_ID")
-private long ristoranteId;
+@Column(name = "UTENTE_ID")
+private long utenteId;
 @Column(name = "NOME")
 private String nome;
-@Column(name = "POSIZIONE")
-private String posizione;
-@Column(name = "POSTI_DISPONIBILI")
-private long postiDisponibili;
-@Column(name = "TIPO_CUCINA")
-private String tipoCucina;
+@Column(name = "COGNOME")
+private String cognome;
 @Column(name = "EMAIL")
-private String email;
+private long email;
+@Column(name = "USER ID")
+private String userId;
+@Column(name = "AFFIDABILITA")
+private String affidabilita;
+public long getUtenteId() {
+	return utenteId;
+}
+public void setUtenteId(long utenteId) {
+	this.utenteId = utenteId;
+}
+public String getNome() {
+	return nome;
+}
+public void setNome(String nome) {
+	this.nome = nome;
+}
+public String getCognome() {
+	return cognome;
+}
+public void setCognome(String cognome) {
+	this.cognome = cognome;
+}
+public long getEmail() {
+	return email;
+}
+public void setEmail(long email) {
+	this.email = email;
+}
+public String getUserId() {
+	return userId;
+}
+public void setUserId(String userId) {
+	this.userId = userId;
+}
+public String getAffidabilita() {
+	return affidabilita;
+}
+public void setAffidabilita(String affidabilita) {
+	this.affidabilita = affidabilita;
+}
+public Green_Utenti(long utenteId, String nome, String cognome, long email, String userId, String affidabilita) {
+	super();
+	this.utenteId = utenteId;
+	this.nome = nome;
+	this.cognome = cognome;
+	this.email = email;
+	this.userId = userId;
+	this.affidabilita = affidabilita;
+}
+public Green_Utenti() {
+	super();
+}
+@Override
+public String toString() {
+	return "Green_Utenti [utenteId=" + utenteId + ", nome=" + nome + ", cognome=" + cognome + ", email=" + email
+			+ ", userId=" + userId + ", affidabilita=" + affidabilita + "]";
+}
+
+
+
+
+
+
+
+
+
+
+
 
 
 }
+
