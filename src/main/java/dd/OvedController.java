c55ef2fff39c0f672c469b2b63c681925f1fdefb
Luca Avino <luca.avino@virgilio.it>
2019-06-03 15:41:15 +0200

    ok

--- src/main/java/dd/OvedController.java
+++ src/main/java/dd/OvedController.java
@@ -93,78 +93,7 @@ public class OvedController {
 	
 	
 	
-@Autowired Green_UtentiRepo repo2;
-	
-	
 
-	
-	@GetMapping("/ristoratore")
-	public String elencoUtenti(Model model) {
-		model.addAttribute("restaurants", repo.findAll()); 
-		return "/elenco";
-	}
-	
-	
-	@GetMapping("/insert")
-	public String insertUtenti() { 
-		return "/inserisci";
-	}
-	
-	
-	@GetMapping("/confirm")
-	public String inserisciUtenti(@RequestParam(name = "id") long ristoranteId, 
-			@RequestParam(name = "nome") String nome, @RequestParam(name = "posizione") String posizione,
-			@RequestParam(name = "postiDisponibili") long postiDisponibili, 
-			@RequestParam(name = "tipoCucina") String tipoCucina,
-			@RequestParam(name="email") String email,
-			
-			Model model) {
-		Green_Ristorante risto= new Green_Ristorante(ristoranteId, nome, posizione, postiDisponibili,  tipoCucina,
-			email);
-		repo.save(risto);
-		model.addAttribute("restaurants", repo.findAll()); /* dopo aver inserito l'elemento dall'oggetto,
-		devo ripassare i valori nell'oggetto repo con findAll */
-		return "/elenco";
-	}
-	
-	
-	@GetMapping("/update")
-	public String updateUtenti() {
-		
-		return "/modifica";
-	}
-	
-	
-	@GetMapping("/modifica")
-	public String modificaUtenti(@RequestParam(name = "id") long ristoranteId, 
-			@RequestParam(name = "nome") String nome, @RequestParam(name = "posizione") String posizione,
-			@RequestParam(name = "postiDisponibili") long postiDisponibili, 
-			@RequestParam(name = "tipoCucina") String tipoCucina,
-			@RequestParam(name="email") String email,
-			
-			Model model) {
-		Green_Ristorante risto= new Green_Ristorante(ristoranteId, nome, posizione, postiDisponibili,  tipoCucina,
-			email);
-		repo.save(risto);
-		model.addAttribute("restaurants", repo.findAll()); /* dopo aver inserito l'elemento dall'oggetto,
-		devo ripassare i valori nell'oggetto repo con findAll */
-		return "/elenco";
-	}
-	
-	
-	
-	
-	@GetMapping("/delete")
-	public String eliminaUtenti(@RequestParam(name = "id") long id, Model model) {
-		repo.deleteById(id);
-		model.addAttribute("restaurants", repo.findAll()); /* dopo aver cancellato l'elemento dall'oggetto,
-		devo ripassare i valori nell'oggetto repo con findAll */
-		return "/elenco";
-	}
-	
-	
-	
-	
 	
 	
 	
